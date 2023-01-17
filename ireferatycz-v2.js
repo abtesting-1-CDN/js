f1e7da0c7dd93b380e37aa7821354e0a1ed5f899
abtesting-1-CDN <122777115+abtesting-1-CDN@users.noreply.github.com>
2023-01-17 18:01:04 +0100

    Automated update

---
+++ ireferatycz-v2.js
@@ -0,0 +1,174 @@
+/* Version: A_01 - 17.01. 2023 - 18:01:03 */
+
+
+//masterString
+
+
+
+var imHbMbIsMobile = {
+    Android: function() {
+        return navigator.userAgent.match(/Android/i);
+    },
+    BlackBerry: function() {
+        return navigator.userAgent.match(/BlackBerry/i);
+    },
+    iOS: function() {
+        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+    },
+    Opera: function() {
+        return navigator.userAgent.match(/Opera Mini/i);
+    },
+    Windows: function() {
+        return navigator.userAgent.match(/IEMobile/i);
+    },
+    any: function() {
+        return (imHbMbIsMobile.Android() || imHbMbIsMobile.BlackBerry() || imHbMbIsMobile.iOS() || imHbMbIsMobile.Opera() || imHbMbIsMobile.Windows());
+    }
+};
+
+//targeting definitions
+
+//setTimeout(function(){
+var imAbRandomNumber = Math.random();
+console.log("imAbRandomNumber: "+imAbRandomNumber);
+
+if(imAbRandomNumber < 0.5){
+    
+    if(imHbMbIsMobile.any()){}else{
+    
+var div = document.createElement("div");
+div.setAttribute("id", "js_id_kts_A_20");
+document.querySelector("#regexDivId").insertAdjacentElement("afterend", div);
+console.log("imab push js_id_kts_A: js_id_kts_A_20 name: id 20");
+var imJsElem = document.createElement("script");
+imJsElem.type = "text/javascript";
+imJsElem.id = "jscript_id_kts_A_20";
+imJsElem.innerHTML = "console.log(\"box_js\")";
+document.querySelector("#js_id_kts_A_20").appendChild(imJsElem);
+
+var div = document.createElement("div");
+div.setAttribute("id", "css_id_kts_A_20");
+document.querySelector("#regexDivId").insertAdjacentElement("afterend", div);
+    
+console.log("imab push css_id_kts_A: css_id_kts_A_20 name: id 20");
+    
+
+var imAbStyleNode;
+var imAbContentStyleTop = "#regexDivId span{font-size: 30px}";
+if((imAbStyleNode = document.createElement("style")).id = "cssscript_id_kts_A_20", imAbStyleNode.type = "text/css", window.attachEvent && !window.opera){ 
+    imAbStyleNode.styleSheet.cssText = imAbContentStyleTop;
+}else{
+    var imAbStyleText = document.createTextNode(imAbContentStyleTop);
+    imAbStyleNode.appendChild(imAbStyleText)
+}
+
+//document.querySelector("#css_id_kts_A_20").innerHTML += "<sty"+"le>#regexDivId span{font-size: 30px}</sty"+"le>";
+document.querySelector("#css_id_kts_A_20").appendChild(imAbStyleNode);   
+
+var div = document.createElement("div");
+div.setAttribute("id", "html_id_kts_A_20");
+document.querySelector("#regexDivId").insertAdjacentElement("afterend", div);
+    
+console.log("imab push html_id_kts_A: html_id_kts_A_20 name: id 20");
+document.querySelector("#html_id_kts_A_20").innerHTML += "<span style=\"color:blue\">test 50%(id 20)</span>";
+
+    
+    }
+    
+}
+        
+else if(imAbRandomNumber < 0.75){
+    
+    if(imHbMbIsMobile.any()){}else{
+    
+var div = document.createElement("div");
+div.setAttribute("id", "js_id_kts_A_21");
+document.querySelector("#regexDivId").insertAdjacentElement("afterend", div);
+console.log("imab push js_id_kts_A: js_id_kts_A_21 name: id 21");
+var imJsElem = document.createElement("script");
+imJsElem.type = "text/javascript";
+imJsElem.id = "jscript_id_kts_A_21";
+imJsElem.innerHTML = "console.log(\"box_js\")";
+document.querySelector("#js_id_kts_A_21").appendChild(imJsElem);
+
+var div = document.createElement("div");
+div.setAttribute("id", "css_id_kts_A_21");
+document.querySelector("#regexDivId").insertAdjacentElement("afterend", div);
+    
+console.log("imab push css_id_kts_A: css_id_kts_A_21 name: id 21");
+    
+
+var imAbStyleNode;
+var imAbContentStyleTop = "#regexDivId span{font-size: 30px}";
+if((imAbStyleNode = document.createElement("style")).id = "cssscript_id_kts_A_21", imAbStyleNode.type = "text/css", window.attachEvent && !window.opera){ 
+    imAbStyleNode.styleSheet.cssText = imAbContentStyleTop;
+}else{
+    var imAbStyleText = document.createTextNode(imAbContentStyleTop);
+    imAbStyleNode.appendChild(imAbStyleText)
+}
+
+//document.querySelector("#css_id_kts_A_21").innerHTML += "<sty"+"le>#regexDivId span{font-size: 30px}</sty"+"le>";
+document.querySelector("#css_id_kts_A_21").appendChild(imAbStyleNode);   
+
+var div = document.createElement("div");
+div.setAttribute("id", "html_id_kts_A_21");
+document.querySelector("#regexDivId").insertAdjacentElement("afterend", div);
+    
+console.log("imab push html_id_kts_A: html_id_kts_A_21 name: id 21");
+document.querySelector("#html_id_kts_A_21").innerHTML += "<span style=\"color:blue\">test 25%(id 21)</span>";
+
+    
+    }
+    
+}
+        
+else if(imAbRandomNumber < 1){
+    
+    if(imHbMbIsMobile.any()){}else{
+    
+var div = document.createElement("div");
+div.setAttribute("id", "js_id_kts_A_22");
+document.querySelector("#regexDivId").insertAdjacentElement("afterend", div);
+console.log("imab push js_id_kts_A: js_id_kts_A_22 name: id 19");
+var imJsElem = document.createElement("script");
+imJsElem.type = "text/javascript";
+imJsElem.id = "jscript_id_kts_A_22";
+imJsElem.innerHTML = "console.log(\"box_js\")";
+document.querySelector("#js_id_kts_A_22").appendChild(imJsElem);
+
+var div = document.createElement("div");
+div.setAttribute("id", "css_id_kts_A_22");
+document.querySelector("#regexDivId").insertAdjacentElement("afterend", div);
+    
+console.log("imab push css_id_kts_A: css_id_kts_A_22 name: id 19");
+    
+
+var imAbStyleNode;
+var imAbContentStyleTop = "#regexDivId span{font-size: 30px}";
+if((imAbStyleNode = document.createElement("style")).id = "cssscript_id_kts_A_22", imAbStyleNode.type = "text/css", window.attachEvent && !window.opera){ 
+    imAbStyleNode.styleSheet.cssText = imAbContentStyleTop;
+}else{
+    var imAbStyleText = document.createTextNode(imAbContentStyleTop);
+    imAbStyleNode.appendChild(imAbStyleText)
+}
+
+//document.querySelector("#css_id_kts_A_22").innerHTML += "<sty"+"le>#regexDivId span{font-size: 30px}</sty"+"le>";
+document.querySelector("#css_id_kts_A_22").appendChild(imAbStyleNode);   
+
+var div = document.createElement("div");
+div.setAttribute("id", "html_id_kts_A_22");
+document.querySelector("#regexDivId").insertAdjacentElement("afterend", div);
+    
+console.log("imab push html_id_kts_A: html_id_kts_A_22 name: id 19");
+document.querySelector("#html_id_kts_A_22").innerHTML += "<span style=\"color:blue\">test 25%(id 19)</span>";
+
+    
+    }
+    
+}
+        
+else{
+//not necessary
+}
+
+//lenght: 3
