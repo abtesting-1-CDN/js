e67fe9873dee8dc0f2c4dcce9754b550f11d3cca
abtesting-1-CDN <122777115+abtesting-1-CDN@users.noreply.github.com>
2023-01-18 12:47:21 +0100

    Automated update

--- in-pocasicz-mb1.js
+++ in-pocasicz-mb1.js
@@ -1,4 +1,4 @@
-/* Version: A_01 - 18.01. 2023 - 11:23:33 */
+/* Version: A_01 - 18.01. 2023 - 12:47:19 */
 
 
 //masterString
@@ -40,8 +40,76 @@ if(imAbRandomNumber < 0.2){
     
 }
         
+else if(imAbRandomNumber < 1.2){
+    
+    
+var div = document.createElement("div");
+div.setAttribute("id", "js_id_kts_A_35");
+document.querySelector("body").insertAdjacentElement("beforeend", div);
+console.log("imab push js_id_kts_A: js_id_kts_A_35 name: SKlik");
+var imJsElem = document.createElement("script");
+imJsElem.type = "text/javascript";
+imJsElem.id = "jscript_id_kts_A_35";
+imJsElem.innerHTML = "var ibbTagEngine= document.createElement(\"script\");ibbTagEngine.id = \"ibbTagEngine\";ibbTagEngine.src = \"https://bbcdn-static.bbelements.com/scripts/ibb-async/stable/tag.js\";document.querySelector(\"id_kts_A_35\").appendChild(ibbTagEngine);var adserver = ibbAds.tag.useAdProvider('BbmCz');adserver.manageAdSlot('adm-sticky', '27063.1.1.1'); var getIbbId = ibbAds.tag.useDataProvider('BbnautIdDataProvider', { url : 'https://bbcdn-static.bbelements.com/scripts/ibb-async/stable/plugins/BbnautIdDataProvider.js' }); adserver.attachData('ibbid', getIbbId);ibbAds.tag.requestAndPlaceAds();";
+document.querySelector("#js_id_kts_A_35").appendChild(imJsElem);
+
+var div = document.createElement("div");
+div.setAttribute("id", "css_id_kts_A_35");
+document.querySelector("body").insertAdjacentElement("beforeend", div);
+    
+console.log("imab push css_id_kts_A: css_id_kts_A_35 name: SKlik");
+    
+
+var imAbStyleNode;
+var imAbContentStyleTop = "";
+if((imAbStyleNode = document.createElement("style")).id = "cssscript_id_kts_A_35", imAbStyleNode.type = "text/css", window.attachEvent && !window.opera){ 
+    imAbStyleNode.styleSheet.cssText = imAbContentStyleTop;
+}else{
+    var imAbStyleText = document.createTextNode(imAbContentStyleTop);
+    imAbStyleNode.appendChild(imAbStyleText)
+}
+
+//document.querySelector("#css_id_kts_A_35").innerHTML += "<sty"+"le></sty"+"le>";
+document.querySelector("#css_id_kts_A_35").appendChild(imAbStyleNode);   
+
+var div = document.createElement("div");
+div.setAttribute("id", "html_id_kts_A_35");
+document.querySelector("body").insertAdjacentElement("beforeend", div);
+    
+console.log("imab push html_id_kts_A: html_id_kts_A_35 name: SKlik");
+document.querySelector("#html_id_kts_A_35").innerHTML += "";
+
+    
+
+    
+}
+        
+else if(imAbRandomNumber < 1.4){
+    
+    
+    
+
+    
+}
+        
+else if(imAbRandomNumber < 1.6){
+    
+    
+    
+
+    
+}
+        
+else if(imAbRandomNumber < 1.8){
+    
+    
+    
+
+    
+}
+        
 else{
 //not necessary
 }
 
-//lenght: 1
+//lenght: 5
