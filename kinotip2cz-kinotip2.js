211e98ab1f1eb07eeeb0f8705f908823a02872f0
abtesting-1-CDN <122777115+abtesting-1-CDN@users.noreply.github.com>
2024-10-03 09:44:23 +0200

    Automated update

---
+++ kinotip2cz-kinotip2.js
@@ -0,0 +1,171 @@
+/* Version: A_01 - 03.10. 2024 - 09:44:20 */
+
+
+//masterString
+
+
+
+var imHbMbIsMobile = {
+    Android: function() {
+        return navigator.userAgent.match(/Android/i);
+    },
+    BlackBerry: function() {
+        return navigator.userAgent.match(/BlackBerry/i);
+    },
+    iOS: function() {
+        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+    },
+    Opera: function() {
+        return navigator.userAgent.match(/Opera Mini/i);
+    },
+    Windows: function() {
+        return navigator.userAgent.match(/IEMobile/i);
+    },
+    any: function() {
+        return (imHbMbIsMobile.Android() || imHbMbIsMobile.BlackBerry() || imHbMbIsMobile.iOS() || imHbMbIsMobile.Opera() || imHbMbIsMobile.Windows());
+    }
+};
+
+//targeting definitions
+
+//setTimeout(function(){
+var imAbRandomNumber = Math.random();
+console.log("imAbRandomNumber: "+imAbRandomNumber);
+
+if(imAbRandomNumber < 0.5){
+    
+    
+var div = document.createElement("div");
+div.setAttribute("id", "html_id_kts_A_87");
+document.querySelector("body").insertAdjacentElement("afterbegin", div);
+    
+console.log("imab push html_id_kts_A: html_id_kts_A_87 name: Performax");
+document.querySelector("#html_id_kts_A_87").innerHTML += "";
+
+var div = document.createElement("div");
+div.setAttribute("id", "css_id_kts_A_87");
+document.querySelector("body").insertAdjacentElement("afterbegin", div);
+    
+console.log("imab push css_id_kts_A: css_id_kts_A_87 name: Performax");
+    
+
+var imAbStyleNode;
+var imAbContentStyleTop = "";
+if((imAbStyleNode = document.createElement("style")).id = "cssscript_id_kts_A_87", imAbStyleNode.type = "text/css", window.attachEvent && !window.opera){ 
+    imAbStyleNode.styleSheet.cssText = imAbContentStyleTop;
+}else{
+    var imAbStyleText = document.createTextNode(imAbContentStyleTop);
+    imAbStyleNode.appendChild(imAbStyleText)
+}
+
+//document.querySelector("#css_id_kts_A_87").innerHTML += "<sty"+"le></sty"+"le>";
+document.querySelector("#css_id_kts_A_87").appendChild(imAbStyleNode);   
+
+var div = document.createElement("div");
+div.setAttribute("id", "js_id_kts_A_87");
+document.querySelector("body").insertAdjacentElement("afterbegin", div);
+console.log("imab push js_id_kts_A: js_id_kts_A_87 name: Performax");
+var imJsElem = document.createElement("script");
+imJsElem.type = "text/javascript";
+imJsElem.id = "jscript_id_kts_A_87";
+imJsElem.innerHTML = "imAbTestIsMobile = {    Android: function() {        return navigator.userAgent.match(/Android/i);    },    BlackBerry: function() {        return navigator.userAgent.match(/BlackBerry/i);    },    iOS: function() {        return navigator.userAgent.match(/iPhone|iPad|iPod/i);    },    Opera: function() {        return navigator.userAgent.match(/Opera Mini/i);    },    Windows: function() {        return navigator.userAgent.match(/IEMobile/i);    },    any: function() {        return (imAbTestIsMobile.Android() || imAbTestIsMobile.BlackBerry() || imAbTestIsMobile.iOS() || imAbTestIsMobile.Opera() || imAbTestIsMobile.Windows());    }};if(imAbTestIsMobile.any()){     imAbRunOnlyOnMobile();}else{     document.body.setAttribute(\"AB\", \"Performax-No-Mobile\");}function imAbRunOnlyOnMobile(){document.body.setAttribute(\"data-mobilniBranding-closeMbCappingPageBrowsing\", \"stop-MbReloadingAds\");document.body.setAttribute(\"AB\", \"Performax\");var imAbPer= document.createElement(\"script\");imAbPer.id = \"imAbPer\";imAbPer.src = \"https://cdn.performax.cz/yi/adsbypx/px_autoads.js\";document.querySelector(\"#html_id_kts_A_87\").appendChild(imAbPer);}var imAbHbMaster = document.createElement(\"script\");imAbHbMaster.type = \"text/javascript\";imAbHbMaster.id = \"imHbMaster\";imAbHbMaster.src = \"https://cdn.jsdelivr.net/gh/HB-4-CDN/js/1771.js\";document.getElementsByTagName('head')[0].appendChild(imAbHbMaster );";
+document.querySelector("#js_id_kts_A_87").appendChild(imJsElem);
+
+    
+
+    
+}
+        
+else if(imAbRandomNumber < 0.75){
+    
+    
+var div = document.createElement("div");
+div.setAttribute("id", "html_id_kts_A_88");
+document.querySelector("body").insertAdjacentElement("afterbegin", div);
+    
+console.log("imab push html_id_kts_A: html_id_kts_A_88 name: R2B2");
+document.querySelector("#html_id_kts_A_88").innerHTML += "";
+
+var div = document.createElement("div");
+div.setAttribute("id", "css_id_kts_A_88");
+document.querySelector("body").insertAdjacentElement("afterbegin", div);
+    
+console.log("imab push css_id_kts_A: css_id_kts_A_88 name: R2B2");
+    
+
+var imAbStyleNode;
+var imAbContentStyleTop = "";
+if((imAbStyleNode = document.createElement("style")).id = "cssscript_id_kts_A_88", imAbStyleNode.type = "text/css", window.attachEvent && !window.opera){ 
+    imAbStyleNode.styleSheet.cssText = imAbContentStyleTop;
+}else{
+    var imAbStyleText = document.createTextNode(imAbContentStyleTop);
+    imAbStyleNode.appendChild(imAbStyleText)
+}
+
+//document.querySelector("#css_id_kts_A_88").innerHTML += "<sty"+"le></sty"+"le>";
+document.querySelector("#css_id_kts_A_88").appendChild(imAbStyleNode);   
+
+var div = document.createElement("div");
+div.setAttribute("id", "js_id_kts_A_88");
+document.querySelector("body").insertAdjacentElement("afterbegin", div);
+console.log("imab push js_id_kts_A: js_id_kts_A_88 name: R2B2");
+var imJsElem = document.createElement("script");
+imJsElem.type = "text/javascript";
+imJsElem.id = "jscript_id_kts_A_88";
+imJsElem.innerHTML = "imAbTestIsMobile = {    Android: function() {        return navigator.userAgent.match(/Android/i);    },    BlackBerry: function() {        return navigator.userAgent.match(/BlackBerry/i);    },    iOS: function() {        return navigator.userAgent.match(/iPhone|iPad|iPod/i);    },    Opera: function() {        return navigator.userAgent.match(/Opera Mini/i);    },    Windows: function() {        return navigator.userAgent.match(/IEMobile/i);    },    any: function() {        return (imAbTestIsMobile.Android() || imAbTestIsMobile.BlackBerry() || imAbTestIsMobile.iOS() || imAbTestIsMobile.Opera() || imAbTestIsMobile.Windows());    }};if(imAbTestIsMobile.any()){     imAbRunOnlyOnMobile();}else{     document.body.setAttribute(\"AB\", \"R2B2-No-Mobile\");}function imAbRunOnlyOnMobile(){document.body.setAttribute(\"data-mobilniBranding-closeMbCappingPageBrowsing\", \"stop-MbReloadingAds\");document.body.setAttribute(\"AB\", \"R2B2\");var imAbR= document.createElement(\"script\");imAbR.id = \"imAbR\";imAbR.src = \"//delivery.r2b2.cz/get/kinotip2.cz/generic/sticky/mobile\";document.querySelector(\"#html_id_kts_A_88\").appendChild(imAbR);}var imAbHbMaster = document.createElement(\"script\");imAbHbMaster.type = \"text/javascript\";imAbHbMaster.id = \"imHbMaster\";imAbHbMaster.src = \"https://cdn.jsdelivr.net/gh/HB-4-CDN/js/1771.js\";document.getElementsByTagName('head')[0].appendChild(imAbHbMaster );";
+document.querySelector("#js_id_kts_A_88").appendChild(imJsElem);
+
+    
+
+    
+}
+        
+else if(imAbRandomNumber < 1){
+    
+    
+var div = document.createElement("div");
+div.setAttribute("id", "html_id_kts_A_89");
+document.querySelector("body").insertAdjacentElement("afterbegin", div);
+    
+console.log("imab push html_id_kts_A: html_id_kts_A_89 name: HB");
+document.querySelector("#html_id_kts_A_89").innerHTML += "";
+
+var div = document.createElement("div");
+div.setAttribute("id", "css_id_kts_A_89");
+document.querySelector("body").insertAdjacentElement("afterbegin", div);
+    
+console.log("imab push css_id_kts_A: css_id_kts_A_89 name: HB");
+    
+
+var imAbStyleNode;
+var imAbContentStyleTop = "";
+if((imAbStyleNode = document.createElement("style")).id = "cssscript_id_kts_A_89", imAbStyleNode.type = "text/css", window.attachEvent && !window.opera){ 
+    imAbStyleNode.styleSheet.cssText = imAbContentStyleTop;
+}else{
+    var imAbStyleText = document.createTextNode(imAbContentStyleTop);
+    imAbStyleNode.appendChild(imAbStyleText)
+}
+
+//document.querySelector("#css_id_kts_A_89").innerHTML += "<sty"+"le></sty"+"le>";
+document.querySelector("#css_id_kts_A_89").appendChild(imAbStyleNode);   
+
+var div = document.createElement("div");
+div.setAttribute("id", "js_id_kts_A_89");
+document.querySelector("body").insertAdjacentElement("afterbegin", div);
+console.log("imab push js_id_kts_A: js_id_kts_A_89 name: HB");
+var imJsElem = document.createElement("script");
+imJsElem.type = "text/javascript";
+imJsElem.id = "jscript_id_kts_A_89";
+imJsElem.innerHTML = "console.log('AB NO stop-MbReloadingAds' );  document.body.setAttribute(\"AB\", \"HB\");var imAbHbMaster = document.createElement(\"script\");imAbHbMaster.type = \"text/javascript\";imAbHbMaster.id = \"imHbMaster\";imAbHbMaster.src = \"https://cdn.jsdelivr.net/gh/HB-4-CDN/js/1777.js\";document.getElementsByTagName('head')[0].appendChild(imAbHbMaster );";
+document.querySelector("#js_id_kts_A_89").appendChild(imJsElem);
+
+    
+
+    
+}
+        
+else{
+//not necessary
+}
+
+//lenght: 3
