a7a32d51020b12174ac99208b66f868caf646328
abtesting-1-CDN <122777115+abtesting-1-CDN@users.noreply.github.com>
2024-10-03 09:47:35 +0200

    Automated update

---
+++ kinotip2cz-kinotip2_new.js
@@ -0,0 +1,63 @@
+/* Version: A_01 - 03.10. 2024 - 09:47:33 */
+
+
+//masterString
+
+
+
+var imHbMbIsMobile = {
+    Android: function() {
+        return navigator.userAgent.match(/Android/i);
+    },
+    BlackBerry: function() {
+        return navigator.userAgent.match(/BlackBerry/i);
+    },
+    iOS: function() {
+        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+    },
+    Opera: function() {
+        return navigator.userAgent.match(/Opera Mini/i);
+    },
+    Windows: function() {
+        return navigator.userAgent.match(/IEMobile/i);
+    },
+    any: function() {
+        return (imHbMbIsMobile.Android() || imHbMbIsMobile.BlackBerry() || imHbMbIsMobile.iOS() || imHbMbIsMobile.Opera() || imHbMbIsMobile.Windows());
+    }
+};
+
+//targeting definitions
+
+//setTimeout(function(){
+var imAbRandomNumber = Math.random();
+console.log("imAbRandomNumber: "+imAbRandomNumber);
+
+if(imAbRandomNumber < 0.8){
+    
+    
+    
+
+    
+}
+        
+else if(imAbRandomNumber < 0.9){
+    
+    
+    
+
+    
+}
+        
+else if(imAbRandomNumber < 1){
+    
+    
+    
+
+    
+}
+        
+else{
+//not necessary
+}
+
+//lenght: 3
