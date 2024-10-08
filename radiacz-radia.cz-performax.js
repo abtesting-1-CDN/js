927cf57f785f6310854cabf2bd6201ab90d631bf
abtesting-1-CDN <122777115+abtesting-1-CDN@users.noreply.github.com>
2024-10-08 13:36:08 +0200

    Automated update

---
+++ radiacz-radia.cz-performax.js
@@ -0,0 +1,127 @@
+/* Version: A_01 - 08.10. 2024 - 13:36:05 */
+
+
+//masterString
+
+
+
+var imHbMbIsMobile = {
+    Android: function() {
+        return navigator.userAgent.match(/Android/i);
+    },
+    BlackBerry: function() {
+        return navigator.userAgent.match(/BlackBerry/i);
+    },
+    iOS: function() {
+        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+    },
+    Opera: function() {
+        return navigator.userAgent.match(/Opera Mini/i);
+    },
+    Windows: function() {
+        return navigator.userAgent.match(/IEMobile/i);
+    },
+    any: function() {
+        return (imHbMbIsMobile.Android() || imHbMbIsMobile.BlackBerry() || imHbMbIsMobile.iOS() || imHbMbIsMobile.Opera() || imHbMbIsMobile.Windows());
+    }
+};
+
+//targeting definitions
+
+//setTimeout(function(){
+var imAbRandomNumber = Math.random();
+console.log("imAbRandomNumber: "+imAbRandomNumber);
+
+if(imAbRandomNumber < 0.9){
+    
+    
+var div = document.createElement("div");
+div.setAttribute("id", "html_id_kts_A_133");
+document.querySelector("body").insertAdjacentElement("afterbegin", div);
+    
+console.log("imab push html_id_kts_A: html_id_kts_A_133 name: Performax");
+document.querySelector("#html_id_kts_A_133").innerHTML += "";
+
+var div = document.createElement("div");
+div.setAttribute("id", "css_id_kts_A_133");
+document.querySelector("body").insertAdjacentElement("afterbegin", div);
+    
+console.log("imab push css_id_kts_A: css_id_kts_A_133 name: Performax");
+    
+
+var imAbStyleNode;
+var imAbContentStyleTop = "";
+if((imAbStyleNode = document.createElement("style")).id = "cssscript_id_kts_A_133", imAbStyleNode.type = "text/css", window.attachEvent && !window.opera){ 
+    imAbStyleNode.styleSheet.cssText = imAbContentStyleTop;
+}else{
+    var imAbStyleText = document.createTextNode(imAbContentStyleTop);
+    imAbStyleNode.appendChild(imAbStyleText)
+}
+
+//document.querySelector("#css_id_kts_A_133").innerHTML += "<sty"+"le></sty"+"le>";
+document.querySelector("#css_id_kts_A_133").appendChild(imAbStyleNode);   
+
+var div = document.createElement("div");
+div.setAttribute("id", "js_id_kts_A_133");
+document.querySelector("body").insertAdjacentElement("afterbegin", div);
+console.log("imab push js_id_kts_A: js_id_kts_A_133 name: Performax");
+var imJsElem = document.createElement("script");
+imJsElem.type = "text/javascript";
+imJsElem.id = "jscript_id_kts_A_133";
+imJsElem.innerHTML = "imAbTestIsMobile = {    Android: function() {        return navigator.userAgent.match(/Android/i);    },    BlackBerry: function() {        return navigator.userAgent.match(/BlackBerry/i);    },    iOS: function() {        return navigator.userAgent.match(/iPhone|iPad|iPod/i);    },    Opera: function() {        return navigator.userAgent.match(/Opera Mini/i);    },    Windows: function() {        return navigator.userAgent.match(/IEMobile/i);    },    any: function() {        return (imAbTestIsMobile.Android() || imAbTestIsMobile.BlackBerry() || imAbTestIsMobile.iOS() || imAbTestIsMobile.Opera() || imAbTestIsMobile.Windows());    }};if(imAbTestIsMobile.any()){     imAbRunOnlyOnMobile();}else{     document.body.setAttribute(\"AB\", \"Performax-No-Mobile\");}function imAbRunOnlyOnMobile(){document.body.setAttribute(\"data-mobilniBranding-closeMbCappingPageBrowsing\", \"stop-MbReloadingAds\");document.body.setAttribute(\"AB\", \"Performax\");var imAbPer= document.createElement(\"script\");imAbPer.id = \"imAbPer\";imAbPer.src = \"https://cdn.performax.cz/yi/adsbypx/px_autoads.js\";document.querySelector(\"#html_id_kts_A_133\").appendChild(imAbPer);}var imAbHbMaster = document.createElement(\"script\");imAbHbMaster.type = \"text/javascript\";imAbHbMaster.id = \"imHbMaster\";imAbHbMaster.src = \"https://cdn.jsdelivr.net/gh/HB-5-CDN/js/1806.js\";document.getElementsByTagName('head')[0].appendChild(imAbHbMaster );";
+document.querySelector("#js_id_kts_A_133").appendChild(imJsElem);
+
+    
+
+    
+}
+        
+else if(imAbRandomNumber < 1){
+    
+    
+var div = document.createElement("div");
+div.setAttribute("id", "html_id_kts_A_134");
+document.querySelector("body").insertAdjacentElement("afterbegin", div);
+    
+console.log("imab push html_id_kts_A: html_id_kts_A_134 name: HB");
+document.querySelector("#html_id_kts_A_134").innerHTML += "";
+
+var div = document.createElement("div");
+div.setAttribute("id", "css_id_kts_A_134");
+document.querySelector("body").insertAdjacentElement("afterbegin", div);
+    
+console.log("imab push css_id_kts_A: css_id_kts_A_134 name: HB");
+    
+
+var imAbStyleNode;
+var imAbContentStyleTop = "";
+if((imAbStyleNode = document.createElement("style")).id = "cssscript_id_kts_A_134", imAbStyleNode.type = "text/css", window.attachEvent && !window.opera){ 
+    imAbStyleNode.styleSheet.cssText = imAbContentStyleTop;
+}else{
+    var imAbStyleText = document.createTextNode(imAbContentStyleTop);
+    imAbStyleNode.appendChild(imAbStyleText)
+}
+
+//document.querySelector("#css_id_kts_A_134").innerHTML += "<sty"+"le></sty"+"le>";
+document.querySelector("#css_id_kts_A_134").appendChild(imAbStyleNode);   
+
+var div = document.createElement("div");
+div.setAttribute("id", "js_id_kts_A_134");
+document.querySelector("body").insertAdjacentElement("afterbegin", div);
+console.log("imab push js_id_kts_A: js_id_kts_A_134 name: HB");
+var imJsElem = document.createElement("script");
+imJsElem.type = "text/javascript";
+imJsElem.id = "jscript_id_kts_A_134";
+imJsElem.innerHTML = "console.log('AB NO stop-MbReloadingAds' );  document.body.setAttribute(\"AB\", \"HB\");var imAbHbMaster = document.createElement(\"script\");imAbHbMaster.type = \"text/javascript\";imAbHbMaster.id = \"imHbMaster\";imAbHbMaster.src = \"https://cdn.jsdelivr.net/gh/HB-4-CDN/js/1758.js\";document.getElementsByTagName('head')[0].appendChild(imAbHbMaster );";
+document.querySelector("#js_id_kts_A_134").appendChild(imJsElem);
+
+    
+
+    
+}
+        
+else{
+//not necessary
+}
+
+//lenght: 2
