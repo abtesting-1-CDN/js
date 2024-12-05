1715e33c5fc896a36da5b9b2c212884042bb297c
abtesting-1-CDN <122777115+abtesting-1-CDN@users.noreply.github.com>
2024-12-05 10:02:05 +0100

    Automated update

---
+++ in-pocasicz-In-pocasi.cz-admaster.js
@@ -0,0 +1,127 @@
+/* Version: A_01 - 05.12. 2024 - 10:02:02 */
+
+
+//masterString
+
+
+
+var imHbMbIsMobile = {
+    Android: function() {
+        return navigator.userAgent.match(/Android/i);
+    },
+    BlackBerry: function() {
+        return navigator.userAgent.match(/BlackBerry/i);
+    },
+    iOS: function() {
+        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+    },
+    Opera: function() {
+        return navigator.userAgent.match(/Opera Mini/i);
+    },
+    Windows: function() {
+        return navigator.userAgent.match(/IEMobile/i);
+    },
+    any: function() {
+        return (imHbMbIsMobile.Android() || imHbMbIsMobile.BlackBerry() || imHbMbIsMobile.iOS() || imHbMbIsMobile.Opera() || imHbMbIsMobile.Windows());
+    }
+};
+
+//targeting definitions
+
+//setTimeout(function(){
+var imAbRandomNumber = Math.random();
+console.log("imAbRandomNumber: "+imAbRandomNumber);
+
+if(imAbRandomNumber < 0.95){
+    
+    
+var div = document.createElement("div");
+div.setAttribute("id", "html_id_kts_A_129");
+document.querySelector("body").insertAdjacentElement("afterbegin", div);
+    
+console.log("imab push html_id_kts_A: html_id_kts_A_129 name: HB");
+document.querySelector("#html_id_kts_A_129").innerHTML += "";
+
+var div = document.createElement("div");
+div.setAttribute("id", "css_id_kts_A_129");
+document.querySelector("body").insertAdjacentElement("afterbegin", div);
+    
+console.log("imab push css_id_kts_A: css_id_kts_A_129 name: HB");
+    
+
+var imAbStyleNode;
+var imAbContentStyleTop = "";
+if((imAbStyleNode = document.createElement("style")).id = "cssscript_id_kts_A_129", imAbStyleNode.type = "text/css", window.attachEvent && !window.opera){ 
+    imAbStyleNode.styleSheet.cssText = imAbContentStyleTop;
+}else{
+    var imAbStyleText = document.createTextNode(imAbContentStyleTop);
+    imAbStyleNode.appendChild(imAbStyleText)
+}
+
+//document.querySelector("#css_id_kts_A_129").innerHTML += "<sty"+"le></sty"+"le>";
+document.querySelector("#css_id_kts_A_129").appendChild(imAbStyleNode);   
+
+var div = document.createElement("div");
+div.setAttribute("id", "js_id_kts_A_129");
+document.querySelector("body").insertAdjacentElement("afterbegin", div);
+console.log("imab push js_id_kts_A: js_id_kts_A_129 name: HB");
+var imJsElem = document.createElement("script");
+imJsElem.type = "text/javascript";
+imJsElem.id = "jscript_id_kts_A_129";
+imJsElem.innerHTML = "console.log('AB NO stop-MbReloadingAds' );  document.body.setAttribute(\"AB\", \"HB\");var imAbHbMaster = document.createElement(\"script\");imAbHbMaster.type = \"text/javascript\";imAbHbMaster.id = \"imHbMaster\";imAbHbMaster.src = \"https://cdn.jsdelivr.net/gh/HB-5-CDN/js/1804.js\";document.getElementsByTagName('head')[0].appendChild(imAbHbMaster );";
+document.querySelector("#js_id_kts_A_129").appendChild(imJsElem);
+
+    
+
+    
+}
+        
+else if(imAbRandomNumber < 1){
+    
+    
+var div = document.createElement("div");
+div.setAttribute("id", "html_id_kts_A_135");
+document.querySelector("body").insertAdjacentElement("afterbegin", div);
+    
+console.log("imab push html_id_kts_A: html_id_kts_A_135 name: Admaster");
+document.querySelector("#html_id_kts_A_135").innerHTML += "<div id=\"adm-sticky\"></div>";
+
+var div = document.createElement("div");
+div.setAttribute("id", "css_id_kts_A_135");
+document.querySelector("body").insertAdjacentElement("afterbegin", div);
+    
+console.log("imab push css_id_kts_A: css_id_kts_A_135 name: Admaster");
+    
+
+var imAbStyleNode;
+var imAbContentStyleTop = "";
+if((imAbStyleNode = document.createElement("style")).id = "cssscript_id_kts_A_135", imAbStyleNode.type = "text/css", window.attachEvent && !window.opera){ 
+    imAbStyleNode.styleSheet.cssText = imAbContentStyleTop;
+}else{
+    var imAbStyleText = document.createTextNode(imAbContentStyleTop);
+    imAbStyleNode.appendChild(imAbStyleText)
+}
+
+//document.querySelector("#css_id_kts_A_135").innerHTML += "<sty"+"le></sty"+"le>";
+document.querySelector("#css_id_kts_A_135").appendChild(imAbStyleNode);   
+
+var div = document.createElement("div");
+div.setAttribute("id", "js_id_kts_A_135");
+document.querySelector("body").insertAdjacentElement("afterbegin", div);
+console.log("imab push js_id_kts_A: js_id_kts_A_135 name: Admaster");
+var imJsElem = document.createElement("script");
+imJsElem.type = "text/javascript";
+imJsElem.id = "jscript_id_kts_A_135";
+imJsElem.innerHTML = "imAbTestIsMobile = {    Android: function() {        return navigator.userAgent.match(/Android/i);    },    BlackBerry: function() {        return navigator.userAgent.match(/BlackBerry/i);    },    iOS: function() {        return navigator.userAgent.match(/iPhone|iPad|iPod/i);    },    Opera: function() {        return navigator.userAgent.match(/Opera Mini/i);    },    Windows: function() {        return navigator.userAgent.match(/IEMobile/i);    },    any: function() {        return (imAbTestIsMobile.Android() || imAbTestIsMobile.BlackBerry() || imAbTestIsMobile.iOS() || imAbTestIsMobile.Opera() || imAbTestIsMobile.Windows());    }};if(imAbTestIsMobile.any()){     imAbRunOnlyOnMobile();}else{     document.body.setAttribute(\"AB\", \"Admaster-No-Mobile\");}function imAbRunOnlyOnMobile(){document.body.setAttribute(\"data-mobilniBranding-closeMbCappingPageBrowsing\", \"stop-MbReloadingAds\");document.body.setAttribute(\"AB\", \"Admaster\");var ibbTagEngine= document.createElement(\"script\");ibbTagEngine.id = \"ibbTagEngine\";ibbTagEngine.src = \"https://ads.richmedia.cz/js/adm-in-pocasi.js\";document.querySelector(\"#html_id_kts_A_135\").appendChild(ibbTagEngine);}var imAbHbMaster = document.createElement(\"script\");imAbHbMaster.type = \"text/javascript\";imAbHbMaster.id = \"imHbMaster\";imAbHbMaster.src = \"https://cdn.jsdelivr.net/gh/HB-5-CDN/js/1804.js\";document.getElementsByTagName('head')[0].appendChild(imAbHbMaster );";
+document.querySelector("#js_id_kts_A_135").appendChild(imJsElem);
+
+    
+
+    
+}
+        
+else{
+//not necessary
+}
+
+//lenght: 2
